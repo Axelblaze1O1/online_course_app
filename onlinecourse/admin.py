3457bad1b78d42b5489574cb15157455ce1740e2
Axelblaze <teotiaarnav3@gmail.com>
2023-07-11 11:09:59 +0530

    models added

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Choice,Submission,Question
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 5
 
 class LessonInline(admin.StackedInline):
     model = Lesson
@@ -23,7 +29,7 @@ class LessonAdmin(admin.ModelAdmin):
 
 
 # <HINT> Register Question and Choice models here
-
+admin.site.register(Question)
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
